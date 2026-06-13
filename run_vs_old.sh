589f4f092fd49cbad4cd6680489717ff571e67a5
Wlegby <gigamullman@gmail.com>
2026-06-14 00:46:29 +0200

    switched to cutechess

--- run_vs_old.sh
+++ run_vs_old.sh
@@ -45,10 +45,11 @@ for ((i = 1; i < LAST_VERSION; i++)); do
 done
 
 # 5. Add the match constraints and UNLIMITED time controls
+# Choose ONE of the options below inside the array:
 CUTECHESS_ARGS+=(
   "-each" "tc=inf"
-  "-rounds" "20"
-  "-concurrency" "8"
+  "-rounds" "4"
+  "-concurrency" "6"
   "-pgnout" "$GAME_ARCHIVE_DIR/games_${NEW_ENGINE_NAME}_vs_older_${DATE}.pgn"
 )
 
