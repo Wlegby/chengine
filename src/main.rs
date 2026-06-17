401f1a68482437555b325dc6892112a322bd1ef4
Wlegby <gigamullman@gmail.com>
2026-06-17 22:29:10 +0200

    changed the duration

--- src/main.rs
+++ src/main.rs
@@ -211,7 +211,7 @@ fn start_uci() {
                             if depth == max_depth - 1 {
                                 let dt = Instant::now() - time_start;
                                 // extend max_depth if it didn't take long enough
-                                if dt < Duration::from_millis(1500) {
+                                if dt < Duration::from_millis(1800) {
                                     max_depth += 1;
                                 }
                             }
